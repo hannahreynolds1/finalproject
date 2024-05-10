5759d807978685a25aee0d6fd39dd008c0866c03
hannahreynolds1 <hjr45@georgetown.edu>
2024-05-10 09:09:24 -0400

    Create .qmd and Outline

--- finalprojectwork.R
+++ finalprojectwork.R
@@ -6,10 +6,63 @@ library(ggmap)
 library(dplyr)
 library(haven)
 
+pakistan_shape_1 <- st_read("/Users/hannahreynolds/Desktop/GitHub/finalproject/gadm36_PAK_0.shp")
 
-irt_scores <- read_dta("/Users/hannahreynolds/Desktop/GitHub/finalproject/school/master/public_child_irt_scores_panel.dta")
+ggplot(data = pakistan_shape_1, aes(geometry = geometry)) +
+  geom_sf() +
+  labs(title = "Map 0") +
+  theme_minimal()
 
-teacher_irt_scores <- read_dta("/Users/hannahreynolds/Desktop/GitHub/finalproject/school/master/public_teacher_irt_scores_panel.dta")
 
 
-public_child <-read_dta("/Users/hannahreynolds/Desktop/GitHub/finalproject/school/school2/public_child2.dta")
+pakistan_shape_1 <- st_read("/Users/hannahreynolds/Desktop/GitHub/finalproject/gadm36_PAK_1.shp")
+
+ggplot(data = pakistan_shape_1, aes(geometry = geometry)) +
+  geom_sf() +
+  labs(title = "Map 1") +
+  theme_minimal()
+
+pakistan_shape_2 <- st_read("/Users/hannahreynolds/Desktop/GitHub/finalproject/gadm36_PAK_2.shp")
+
+ggplot(data = pakistan_shape_2, aes(geometry = geometry)) +
+  geom_sf() +
+  labs(title = "Map 2") +
+  theme_minimal()
+
+pakistan_shape_3 <- st_read("/Users/hannahreynolds/Desktop/GitHub/finalproject/gadm36_PAK_3.shp")
+
+ggplot(data = pakistan_shape_3, aes(geometry = geometry)) +
+  geom_sf() +
+  labs(title = "Map 3") +
+  theme_minimal()
+
+
+
+
+
+scores_train %>%
+  ggplot( aes(factor(father_educ), eng_theta_eap)) +
+  geom_boxplot() +
+  labs(title = "Effect of Maternal Education Levels on Math Test Scores")+
+  theme_minimal()
+
+
+```{r}
+avg_scores <- scores_train %>%
+  filter(age > 4) %>%
+  group_by(age) %>%
+  summarize(avg_math_score = mean(math_theta_eap))
+
+# Plot the average math score per age group
+ggplot(avg_scores, aes(x = age, y = avg_math_score)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Average Math Test Scores by Age") +
+  theme_minimal()
+
+avg_scores %>%
+  ggplot(aes(factor(age), avg_math_score)) +
+  geom_boxplot() +
+  labs(title = "Effect of Maternal Education Levels on Math Test Scores")+
+  theme_minimal()
+
+
